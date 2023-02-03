73c0b1d1f2fe487d73657fcc8d0a1ff026dbba47
min-chocopie <minchocopie.lee@gmail.com>
2023-02-03 20:45:27 +0900

    fix: ca 작동 안하는거 수정
    autoscaling의 iam 정책이 제대로 되어있지 않았서 작동을 안했음
    module에서 attach 하는 정책은 SetDesiredCapacity, TerminateInstanceInAutoScalingGroup 이 두개가 없음
    그래서 module 말고 정책 수동으로 만들어서 붙여줌!

--- Addon/CA/main.tf
+++ Addon/CA/main.tf
@@ -1,35 +1,65 @@
-# module "cluster_autoscaler_role" {
-#   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  
-#   role_name                        = "cluster-autoscaler"
-#   attach_cluster_autoscaler_policy = true
+data "aws_iam_policy_document" "cluster_autoscaler" {
+  statement {
+    effect  = "Allow"
+    actions = [
+      "autoscaling:DescribeAutoScalingGroups",
+      "autoscaling:DescribeAutoScalingInstances",
+      "autoscaling:DescribeLaunchConfigurations",
+      "autoscaling:DescribeTags",
+      "ec2:DescribeLaunchTemplateVersions",
+    ]
+    resources = ["*"]
+  }
 
-#   oidc_providers = {
-#     main = {
-#       provider_arn               = data.terraform_remote_state.eks.outputs.oidc_provider_arn
-#       namespace_service_accounts = ["kube-system:cluster-autoscaler"]
-#     }
-#   }
-# }
+  statement {
+    effect  = "Allow"
+    actions = [
+      "autoscaling:SetDesiredCapacity",
+      "autoscaling:TerminateInstanceInAutoScalingGroup",
+      "autoscaling:UpdateAutoScalingGroup",
+    ]
+    resources = ["*"]
 
-# resource "helm_release" "cluster_autoscaler-back" {
-#   name             = "cluster-autoscaler"
-#   namespace        = "kube-system"
-#   repository       = "https://kubernetes.github.io/autoscaler"
-#   chart            = "cluster-autoscaler"
+    condition {
+      test     = "StringEquals"
+      variable = "autoscaling:ResourceTag/k8s.io/cluster-autoscaler/${module.eks.cluster_id}"
+      values   = ["owned"]
+    }
+  }
+}
 
-#   dynamic "set" {
-#     for_each = {
-#       "awsRegion"                                                      = var.region
-#       "autoDiscovery.clusterName"                                      = data.terraform_remote_state.eks.outputs.cluster_name
-#       "autoDiscovery.enabled"                                          = true
-#       "rbac.create"                                                    = true
-#       "rbac.serviceAccount.name"                                       = "cluster-autoscaler"
-#       "rbac.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn" = module.cluster_autoscaler_role.iam_role_arn
-#     }
-#     content {
-#       name  = set.key
-#       value = set.value
-#     }
-#   }
-# }
+resource "aws_iam_policy" "cluster_autoscaler" {
+  name_prefix = "cluster-autoscaler"
+  policy      = data.aws_iam_policy_document.cluster_autoscaler.json
+}
+
+module "cluster_autoscaler_role" {
+  source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  create_role                   = true
+  role_name                     = "cluster-autoscaler"
+  provider_url                  = replace(data.terraform_remote_state.eks.outputs.oidc_provider_url, "https://", "")
+  role_policy_arns              = [aws_iam_policy.cluster_autoscaler.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:cluster-autoscaler"]
+}
+
+resource "helm_release" "cluster_autoscaler" {
+  name             = "cluster-autoscaler"
+  namespace        = "kube-system"
+  repository       = "https://kubernetes.github.io/autoscaler"
+  chart            = "cluster-autoscaler"
+
+  dynamic "set" {
+    for_each = {
+      "awsRegion"                                                      = var.region
+      "autoDiscovery.clusterName"                                      = data.terraform_remote_state.eks.outputs.cluster_name
+      "autoDiscovery.enabled"                                          = true
+      "rbac.create"                                                    = true
+      "rbac.serviceAccount.name"                                       = "cluster-autoscaler"
+      "rbac.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn" = module.cluster_autoscaler_role.iam_role_arn
+    }
+    content {
+      name  = set.key
+      value = set.value
+    }
+  }
+}
--- EKS/outputs.tf
+++ EKS/outputs.tf
@@ -2,6 +2,10 @@ output "oidc_provider_arn" {
   value = module.eks.oidc_provider_arn
 }
 
+output "oidc_provider_url" {
+  value = module.eks.cluster_oidc_issuer_url
+}
+
 output "cluster_name" {
   value = module.eks.cluster_name
 }
