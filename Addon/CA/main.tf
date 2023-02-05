7e1f50d956358d2c102f6a34196e925095a5e8d6
min-chocopie <minchocopie.lee@gmail.com>
2023-02-05 15:16:25 +0900

    fix: output값 data에서 받아오도록 수정

--- Addon/CA/main.tf
+++ Addon/CA/main.tf
@@ -22,7 +22,7 @@ data "aws_iam_policy_document" "cluster_autoscaler" {
 
     condition {
       test     = "StringEquals"
-      variable = "autoscaling:ResourceTag/k8s.io/cluster-autoscaler/${module.eks.cluster_id}"
+      variable = "autoscaling:ResourceTag/k8s.io/cluster-autoscaler/${data.terraform_remote_state.eks.outputs.cluster_name}"
       values   = ["owned"]
     }
   }
@@ -63,3 +63,39 @@ resource "helm_release" "cluster_autoscaler" {
     }
   }
 }
+
+# module "cluster_autoscaler_role" {
+#   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  
+#   role_name                        = "cluster-autoscaler"
+#   attach_cluster_autoscaler_policy = true
+
+#   oidc_providers = {
+#     main = {
+#       provider_arn               = data.terraform_remote_state.eks.outputs.oidc_provider_arn
+#       namespace_service_accounts = ["kube-system:cluster-autoscaler"]
+#     }
+#   }
+# }
+
+# resource "helm_release" "cluster_autoscaler-back" {
+#   name             = "cluster-autoscaler"
+#   namespace        = "kube-system"
+#   repository       = "https://kubernetes.github.io/autoscaler"
+#   chart            = "cluster-autoscaler"
+
+#   dynamic "set" {
+#     for_each = {
+#       "awsRegion"                                                      = var.region
+#       "rbac.serviceAccount.name"                                       = "cluster-autoscaler"
+#       "rbac.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn" = module.cluster_autoscaler_role.iam_role_arn
+#       "autoDiscovery.clusterName"                                      = data.terraform_remote_state.eks.outputs.cluster_name
+#       "autoDiscovery.enabled"                                          = true
+#       "rbac.create"                                                    = true
+#     }
+#     content {
+#       name  = set.key
+#       value = set.value
+#     }
+#   }
+# }
